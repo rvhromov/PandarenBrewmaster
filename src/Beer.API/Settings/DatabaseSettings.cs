3800c24cc88a96d03b0c67579115db909bcb88bf
r_hromov <r_hromov@mifprojects.com>
2021-10-31 20:32:52 +0200

    Updated database settings

--- src/Beer.API/Settings/DatabaseSettings.cs
+++ src/Beer.API/Settings/DatabaseSettings.cs
@@ -2,8 +2,22 @@
 {
     public class DatabaseSettings
     {
-        public string ConnectionString { get; set; }
         public string DatabaseName { get; set; }
         public string CollectionName { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+        
+        public string ConnectionString 
+        {
+            get 
+            {
+                if (string.IsNullOrEmpty(User) || string.IsNullOrEmpty(Password))
+                    return $@"mongodb://{Host}:{Port}";
+                
+                return $@"mongodb://{User}:{Password}@{Host}:{Port}";
+            }
+        }
     }
 }
